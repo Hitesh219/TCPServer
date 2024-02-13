eeb2dc074a939f37d13c197399dc4f3bbb808997
Hitesh219 <114565039+Hitesh219@users.noreply.github.com>
2024-02-13 17:47:05 +0530

    Update main.cpp

--- src/parallel/main.cpp
+++ src/parallel/main.cpp
@@ -64,7 +64,7 @@ int main(int argc, char **argv)
   my_addr.sin_port = htons(portno);
   bind(connection_sock, (struct sockaddr *)&my_addr, sizeof(struct sockaddr_in));
   int a = 1;
-  setsockopt(connection_sock, SOL_SOCKET, SO_REUSEADDR, &a, sizeof a);
+  setsockopt(connection_sock, SOL_SOCKET, SO_REUSEADDR, &a, sizeof (a));
   listen(connection_sock, 100);
 
   int client_len = sizeof(client_addr);
